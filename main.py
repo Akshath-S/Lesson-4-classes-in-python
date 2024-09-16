444ab1b28293b4be5551636a0c455ad4d9201ad7
Akshah-S <akshath.sriram@gmail.com>
2024-09-16 20:26:22 +0530

    Classes in python

---
+++ main.py
@@ -0,0 +1,24 @@
+# class students:
+#     grade=11
+#     print("I am Akshath,I am",grade)
+
+# obj=students()
+
+#activity2
+class student:
+    grade=11
+    name="Akshath"
+    print("Hello i am a student")
+
+    def intro(self):
+        print("my name is",self.name)
+    def details(self):
+        print("i am from grade",self.grade)
+    
+obj=student()
+obj.intro()
+obj.details()
+
+class parrot:
+    species=birds
+    
